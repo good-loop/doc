129072dc4aeeb76980ca687bde516c4000366430
DA <daniel.appel.winterwell@gmail.com>
2018-05-09 12:22:39 +0100

    added a site updater script

---
+++ update-site.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Updating the Website..."
+echo ""
+echo "Robinson is pulling the latest changes from github.com..."
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com gc --prune=now'
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com pull origin master'
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com reset --hard FETCH_HEAD'
+echo ""
+echo "Robinson is converting markdown to HTML..."
+ssh winterwell@robinson.soda.sh 'cd /home/winterwell/doc.good-loop.com/jerbil && bash run-jerbil.sh'
+echo ""
+echo "Robinson is optimising images..."
+ssh winterwell@robinson.soda.sh 'jpegoptim /home/winterwell/doc.good-loop.com/webroot/img/*.jpg'
+ssh winterwell@robinson.soda.sh 'optipng /home/winterwell/doc.good-loop.com/webroot/img/*.png'
+echo ""
+echo "website updated"
