5245da2941bb151d979bd6e6c2b2f1bd4ababc3d
DA <daniel.appel.winterwell@gmail.com>
2018-06-04 12:39:02 +0100

    fixed an oversight on the path to Jerbil and the site itself

--- run-jerbil.sh
+++ run-jerbil.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/
-cd ~/winterwell/jerbil/ && java -cp jerbil.jar:lib/* Jerbil ~/winterwell/doc.good-loop.com
+cd /home/winterwell/jerbil/ && java -cp jerbil.jar:lib/* Jerbil /home/winterwell/doc.good-loop.com
