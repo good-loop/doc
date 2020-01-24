2a747014707c5e5a321caf0c88eca9d6ead99397
DA <daniel.appel.winterwell@gmail.com>
2020-01-24 18:24:50 +0000

    fixed depreciated methods and weirdly in-effective calls to/for jerbil

--- teamcity.update.site.sh
+++ teamcity.update.site.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 printf "\nUpdating the Website...\n"
 printf "\nRobinson is pulling the latest changes from github.com...\n"
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil gc --prune=now'
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil pull origin master'
+ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil reset --hard FETCH_HEAD'
+
 ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com gc --prune=now'
 ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com pull origin master'
 ssh winterwell@robinson.soda.sh 'git --git-dir=/home/winterwell/doc.good-loop.com/.git/ --work-tree=/home/winterwell/doc.good-loop.com reset --hard FETCH_HEAD'
 printf "\nRobinson is converting markdown to HTML...\n"
-ssh winterwell@robinson.soda.sh 'cd /home/winterwell/doc.good-loop.com/jerbil && bash run-jerbil.sh'
+ssh winterwell@robinson.soda.sh "cd /home/winterwell/doc.good-loop.com && java -jar /home/winterwell/jerbil/jerbil-all.jar"
+
 printf "\nRobinson is optimising images...\n"
 ssh winterwell@robinson.soda.sh 'jpegoptim /home/winterwell/doc.good-loop.com/webroot/img/*.jpg'
 ssh winterwell@robinson.soda.sh 'optipng /home/winterwell/doc.good-loop.com/webroot/img/*.png'
