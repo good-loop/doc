ed3ad7d15b76a41b152ec5f5c72bc4b634f07c22
DA <daniel.appel.winterwell@gmail.com>
2021-06-11 15:39:28 +0100

    adding the ability to use less->css compilation

---
+++ build-site.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+jerbil
+lessc src/style/main.less webroot/css/main.css
--- run-jerbil.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/
-# paths as for robinson server
-java -jar ~/jerbil/jerbil-all.jar ~/doc.good-loop.com
