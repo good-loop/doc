65c0fbbabd59bce9dc7b5661bf859f206909815f
athomson0 <aidan@good-loop.com>
2023-05-18 15:46:40 +0100

    update CI build script

--- teamcity.ci.builder.doc.sh
+++ teamcity.ci.builder.doc.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-ssh winterwell@dns-02.good-loop.com bash <<EOF 
-/home/winterwell/config/build-scripts/builder.sh \
-BUILD_TYPE="CI" \
-PROJECT_NAME="doc" \
-BRANCH_NAME="master" \
-NAME_OF_SERVICE="doc" \
-GIT_REPO_URL="github.com:good-loop/doc" \
-PROJECT_ROOT_ON_SERVER="/home/winterwell/doc" \
-PROJECT_USES_BOB="no" \
-PROJECT_USES_NPM="no" \
-PROJECT_USES_WEBPACK="no" \
-PROJECT_USES_JERBIL="yes" \
-PROJECT_USES_WWAPPBASE_SYMLINK="yes"
-EOF
+cd /home/winterwell/wwappbase.js && git checkout master && git pull
+cd /home/winterwell/doc && git checkout master && git pull && jerbil
